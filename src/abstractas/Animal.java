f654f8614b86683c1a74c6e8130e5ae5ab5dabdb
JEFFERSON CHICAIZA <84757114+jechicaiza3@users.noreply.github.com>
2021-08-02 22:40:07 -0500

    Update Animal.java

--- src/abstractas/Animal.java
+++ src/abstractas/Animal.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package abstractas;
 
-/**
- *
- * @author kevin
- */
+
 public abstract class Animal extends SerVivo {
     
     @Override
